209dba760b5e24db78ac34e2a3e17d3956eab2b7
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-10-02 23:00:54 +0900

    cretate components

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  const componentName = "Header";
+
+  return (
+    <div className="test-wrapper">
+      このコンポーネントの名前は{componentName}です。
+    </div>
+  );
+};
+
+export default Header;
