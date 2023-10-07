27c6e2e1ecc7b3c837404c9593f43a3f53c4448b
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-10-08 01:35:40 +0900

    create temp header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,32 @@
 import React from "react";
+import CoverImage from "../images/cover-image.jpg";
+import ProfileImage from "../images/profile-image.png";
 
 const Header = () => {
   const componentName = "Header";
-
   return (
-    <div className="test-wrapper">
-      このコンポーネントの名前は{componentName}です。
-    </div>
+    <header
+      className="main-cover"
+      style={{ backgroundImage: `url(${CoverImage})` }}
+    >
+      <div className="overlay"></div>
+      <div className="container">
+        <div className="display-table">
+          <div className="display-table-contents">
+            <div
+              className="profile-thumb"
+              style={{ backgroundImage: `url(${ProfileImage})` }}
+            ></div>
+            <h1 className="title-text">すがわら</h1>
+            <h3 className="title-text">System Engineer</h3>
+            <ul className="social-icons">
+              <li className="icon-link">{/* TODO */}</li>
+              <li className="icon-link">{/* TODO */}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 };
 
